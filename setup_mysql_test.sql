d38c460ada801a5281a376fc3382b800880a3875
masha-a-m <escritoramaree@gmail.com>
2023-06-21 07:54:55 -0700

    u

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,16 +1,11 @@
 --script that prepares a MySQL server for the project
 
-
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
-
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
 -- grant the user access
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
 --grant proiviledges
 GRANT SELECT ON  performance_schema.* TO 'hbnb_test'@'localhost';
 
--- task sql
 FLUSH PRIVILEGES;
