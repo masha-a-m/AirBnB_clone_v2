f3d341806f4cd048931307c3ce784c081862c38b
masha-a-m <escritoramaree@gmail.com>
2023-06-21 07:50:13 -0700

    u

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- SQL database. SQOL project
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- task airbnb clone project
+
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- 
+GRANT SELECT ON  performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
